a51d25bab6987a36c82af52c21add5e47998ee7d
Nicholas Kiesel <Nicholas Kiesel>
2017-03-25 12:24:43 -0400

    reads nfc card and copies uid of card into uuid of ibeacon from bluetooth module

---
+++ beaconNfcCopy.ino
@@ -0,0 +1,344 @@
+/*********************************************************************
+ This is an example for our nRF51822 based Bluefruit LE modules
+
+ Pick one up today in the adafruit shop!
+
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+
+ MIT license, check LICENSE for more information
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <stdio.h>
+#include <Adafruit_PN532.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+  #include <SoftwareSerial.h>
+#endif
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+/*=========================================================================
+    APPLICATION SETTINGS
+
+    FACTORYRESET_ENABLE     Perform a factory reset when running this sketch
+    
+                            Enabling this will put your Bluefruit LE module
+                            in a 'known good' state and clear any config
+                            data set in previous sketches or projects, so
+                            running this at least once is a good idea.
+   
+                            When deploying your project, however, you will
+                            want to disable factory reset by setting this
+                            value to 0.  If you are making changes to your
+                            Bluefruit LE device via AT commands, and those
+                            changes aren't persisting across resets, this
+                            is the reason why.  Factory reset will erase
+                            the non-volatile memory where config data is
+                            stored, setting it back to factory default
+                            values.
+       
+                            Some sketches that require you to bond to a
+                            central device (HID mouse, keyboard, etc.)
+                            won't work at all with this feature enabled
+                            since the factory reset will clear all of the
+                            bonding data stored on the chip, meaning the
+                            central device won't be able to reconnect.
+
+    BEACON_MANUFACTURER_ID  Company Identifier assigned by Bluetooth SIG
+                            Full list of Manufacturer ID can be found here
+                            https://www.bluetooth.org/en-us/specification/assigned-numbers/company-identifiers
+    BEACON_UUID             16-bytes UUID in hex format AA-BB-...
+    BEACON_MAJOR            16-bit major nunber
+    BEACON_MINOR            16-bit minor nunber
+    BEACON_RSSI_1M
+    -----------------------------------------------------------------------*/
+    #define FACTORYRESET_ENABLE      1
+
+    #define MANUFACTURER_APPLE         "0x004C"
+    #define MANUFACTURER_NORDIC        "0x0059"
+
+    #define BEACON_MANUFACTURER_ID     MANUFACTURER_APPLE
+    #define BEACON_UUID                "00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00"
+    #define BEACON_MAJOR               "0x0000"
+    #define BEACON_MINOR               "0x0000"
+    #define BEACON_RSSI_1M             "-54"
+
+/*
+ * Definitions for the NFC module
+ */
+// If using the breakout with SPI, define the pins for SPI communication.
+#define PN532_SCK  (2)
+#define PN532_MOSI (3)
+#define PN532_SS   (9)
+#define PN532_MISO (5)
+
+// If using the breakout or shield with I2C, define just the pins connected
+// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
+#define PN532_IRQ   (2)
+#define PN532_RESET (3)  // Not connected by default on the NFC Shield
+
+#define DELAY_TIME 400000
+
+/*=========================================================================*/
+
+// Create the bluefruit object, either software serial...uncomment these lines
+/*
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+*/
+
+/* ...or hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
+// Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+/* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
+//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
+//                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
+//                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+// Create nfc object
+
+// Use this line for a breakout with a hardware SPI connection.  Note that
+// the PN532 SCK, MOSI, and MISO pins need to be connected to the Arduino's
+// hardware SPI SCK, MOSI, and MISO pins.  On an Arduino Uno these are
+// SCK = 13, MOSI = 11, MISO = 12.  The SS line can be any digital IO pin.
+//Adafruit_PN532 nfc(PN532_SS);
+
+// Use this line for a breakout with a software SPI connection (recommended):
+Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
+
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW an the BLE module (this function is called
+            automatically on startup)
+*/
+/**************************************************************************/
+void setup(void)
+{
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
+
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit Beacon Example"));
+  Serial.println(F("---------------------------------"));
+
+  Serial.println(F("Initializing the PN532 NFC board"));
+  nfc.begin();
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+  Serial.println("Waiting for an ISO14443A Card ...");
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+//  Serial.println(F("Setting beacon configuration details: "));
+//
+//  // AT+BLEBEACON=0x004C,01-12-23-34-45-56-67-78-89-9A-AB-BC-CD-DE-EF-F0,0x0000,0x0000,-54
+//  ble.print("AT+BLEBEACON="        );
+//  ble.print(BEACON_MANUFACTURER_ID ); ble.print(',');
+//  ble.print(BEACON_UUID            ); ble.print(',');
+//  ble.print(BEACON_MAJOR           ); ble.print(',');
+//  ble.print(BEACON_MINOR           ); ble.print(',');
+//  ble.print(BEACON_RSSI_1M         );
+//  ble.println(); // print line causes the command to execute
+//
+//  // check response status
+//  if (! ble.waitForOK() ) {
+//    error(F("Didn't get the OK"));
+//  }
+//
+  Serial.println();
+//  Serial.println(F("Open your beacon app to test"));
+}
+
+/**************************************************************************/
+/*!
+    @brief  Constantly poll for new command or response data
+*/
+/**************************************************************************/
+void loop(void) {
+  uint8_t success;
+  static uint8_t first = 1;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  static uint32_t timer = DELAY_TIME;
+  
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  if (first) {
+    Serial.println("Waiting for an ISO14443A Card ...");
+    success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+    first = 0;
+  } else {
+    success = 0;
+    timer--;
+  }
+
+  if (!timer) {
+    first = 1;
+    timer = DELAY_TIME;
+    if ( FACTORYRESET_ENABLE ) {
+      /* Perform a factory reset to make sure everything is in a known state */
+      Serial.println(F("Performing a factory reset: "));
+      if ( ! ble.factoryReset() ){
+        error(F("Couldn't factory reset"));
+      }
+    }
+
+    /* Disable command echo from Bluefruit */
+    ble.echo(false);
+  }
+
+  if (success) {
+    // Display some basic information about the card
+    Serial.println("Found an ISO14443A card");
+    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("  UID Value: ");
+    nfc.PrintHex(uid, uidLength);
+    Serial.println("");
+    
+    if (uidLength == 4)
+    {
+      // We probably have a Mifare Classic card ... 
+      Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
+    
+      // Now we need to try to authenticate it for read/write access
+      // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
+      Serial.println("Trying to authenticate block 4 with default KEYA value");
+      uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+    
+    // Start with block 4 (the first block of sector 1) since sector 0
+    // contains the manufacturer data and it's probably better just
+    // to leave it alone unless you know what you're doing
+      success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
+    
+      if (success)
+      {
+        Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
+        uint8_t data[16];
+    
+        // If you want to write something to block 4 to test with, uncomment
+    // the following line and this text should be read back in a minute
+        //memcpy(data, (const uint8_t[]){ 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0 }, sizeof data);
+        // success = nfc.mifareclassic_WriteDataBlock (4, data);
+
+        // Try to read the contents of block 4
+        success = nfc.mifareclassic_ReadDataBlock(4, data);
+    
+        if (success)
+        {
+          // Data seems to have been read ... spit it out
+          Serial.println("Reading Block 4:");
+          nfc.PrintHexChar(data, 16);
+          Serial.println("");
+      
+          // Wait a bit before reading the card again
+          delay(1000);
+        }
+        else
+        {
+          Serial.println("Ooops ... unable to read the requested block.  Try another key?");
+        }
+      }
+      else
+      {
+        Serial.println("Ooops ... authentication failed: Try another key?");
+      }
+    }
+    
+    if (uidLength == 7)
+    {
+      // We probably have a Mifare Ultralight card ...
+      Serial.println("Seems to be a Mifare Ultralight tag (7 byte UID)");
+    
+      // Try to read the first general-purpose user page (#4)
+      Serial.println("Reading page 4");
+      uint8_t data[32];
+      success = nfc.mifareultralight_ReadPage (4, data);
+      if (success)
+      {
+        // Data seems to have been read ... spit it out
+        nfc.PrintHexChar(data, 4);
+        Serial.println("");
+    
+        // Wait a bit before reading the card again
+        delay(1000);
+      }
+      else
+      {
+        Serial.println("Ooops ... unable to read the requested page!?");
+      }
+    }
+    
+    Serial.println("Changing last bytes of uuid to match Business Sync tag");
+    Serial.println(F("Setting beacon configuration details: "));
+    
+    // AT+BLEBEACON=0x004C,01-12-23-34-45-56-67-78-89-9A-AB-BC-CD-DE-EF-F0,0x0000,0x0000,-54
+    char nfcUuid[48] = BEACON_UUID;
+    for (int i = 47-(uidLength*3); i < 47; i+=3) {
+      snprintf(&nfcUuid[i], 4, "-%02X", uid[(i-(47-(uidLength*3))+1)/3]);
+    }
+    
+    ble.print("AT+BLEBEACON="        );
+    ble.print(BEACON_MANUFACTURER_ID ); ble.print(',');
+    ble.print(nfcUuid                ); ble.print(',');
+    ble.print(BEACON_MAJOR           ); ble.print(',');
+    ble.print(BEACON_MINOR           ); ble.print(',');
+    ble.print(BEACON_RSSI_1M         );
+    ble.println(); // print line causes the command to execute
+    
+    // check response status
+    if (! ble.waitForOK() ) {
+      error(F("Didn't get the OK"));
+    }
+  }
+}
